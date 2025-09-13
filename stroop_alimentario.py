0b60af6dcc8f460ce4deea4e1d5def9cac252617
sebastianmorales-lab <sebamorales@fcien.edu.uy>
2025-09-13 12:10:25 -0300

    Stroop corregido

--- stroop_alimentario.py
+++ stroop_alimentario.py
@@ -10,7 +10,7 @@ If you publish work using this script the most relevant publication is:
         https://doi.org/10.3758/s13428-018-01193-y
 
 """
-
+#Actualizado Setiembre 2025
 # --- Import packages ---
 from psychopy import locale_setup
 from psychopy import prefs
